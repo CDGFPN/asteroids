96031b2f64483a472ffef4d9c83140a24127ebbe
CDGFPN <fernandopiresnogueira@gmail.com>
2025-04-05 14:47:55 -0300

    Adicionando a classe de circulo

--- main.py
+++ main.py
@@ -5,12 +5,17 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    while (1):
+    fps_clock = pygame.time.Clock()  
+    FPS = 60
+    dt = 0
+    running = True
+    while (running):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill((0, 0, 0))
         pygame.display.flip()
+        dt = fps_clock.tick(FPS)/1000
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
